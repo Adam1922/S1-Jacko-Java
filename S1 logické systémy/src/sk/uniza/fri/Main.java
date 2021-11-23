126079d5a4216c0196a72ce725b51f606522d9dc
Adam1922 <94933824+Adam1922@users.noreply.github.com>
2021-11-23 22:13:34 +0100

    Add files via upload

--- S1 logické systémy/src/sk/uniza/fri/Main.java
+++ S1 logické systémy/src/sk/uniza/fri/Main.java
@@ -10,31 +10,27 @@ import java.util.Scanner;
  */
 public class Main {
 
+    private static Stavy stavy = new Stavy();
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Stavy stavy = new Stavy();
-        Scanner scanner = new Scanner(System.in);
-
         info();
-
         String vstup = scanner.next();
 
         while (!vstup.equals("k")) {
             if (stavy.zmenaStavuASemaforu(vstup)) {
                 stavy.zmenFarbuSemaforu();
-                System.out.println("Aktuálny stav : " + stavy.getStav());
-                System.out.println("Farba semafóru : " + stavy.getFarbaSemaforu());
                 info();
                 vstup = scanner.next();
             } else {
-                System.out.println("Takýto prechod medzi stavmi NEEXISTUJE, alebo ste zadali nesprávny formát");
-                System.out.println("Aktuálny stav : S" + stavy.getStav());
                 info();
                 vstup = scanner.next();
             }
-
         }
     }
-    public static void info() {
+    private static void info() {
+        System.out.println("Aktuálny stav : S" + stavy.getStav());
+        System.out.println("Farba semafóru : " + stavy.getFarbaSemaforu());
         System.out.println("Prosím zadajte vstup (A,B). Vstup je potrebné zadávať v tvare : a,b ");
         System.out.println("Ak chcete program ukončiť prosím zadajte písmeno k a stalčte klávesu ENTER.");
     }
